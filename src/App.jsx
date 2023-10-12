8e94fd92f3d0d354fdf926103a748bbd2c8fcc61
afruzaakter <afruzaaktersonia1995@gmail.com>
2023-10-12 13:04:15 +0600

    footer add .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from './components/About'
 import Banner from './components/Banner'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 // import Gallery from './components/Gallery';
 import Header from './components/Header'
 import Nav from './components/Nav'
@@ -10,7 +11,8 @@ import Work from './components/Work'
 
 function App() {
   return (
-    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+    <div  className='flex flex-col'>
+      <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
       <Header />
       <Banner />
       <Nav />
@@ -19,8 +21,14 @@ function App() {
       <Work />
       {/* <Gallery/> */}
       <Contact />
+    
       {/* <div className='h-[4000px]'></div> */}
+      </div>
+      <div>
+      <Footer/>
+      </div>
     </div>
+    
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+
+//icons
+import {  FaYoutube, FaLinkedin, FaFacebook ,FaTwitter} from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='waves'>
+                <div className='wave' id="wave1"></div>
+                <div className='wave' id="wave2"></div>
+                <div className='wave' id="wave3"></div>
+                <div className='wave' id="wave4"></div>
+            </div>
+            <div className="footer flex-col gap-y-5">
+                    {/* social icon  */}
+                  <ul className="flex justify-center 
+                   cursor-pointer items-center relative mr-10px gap-x-7 text-[28px]">
+                    <li> <a className=' social-icon' href="#"></a> <FaFacebook/> </li>
+                    <li> <a className=' social-icon'  href="#"></a> <FaYoutube/> </li>
+                    <li> <a className=' social-icon'  href="#"></a><FaLinkedin/> </li>
+                    <li> <a className=' social-icon'  href="#"></a> <FaTwitter/> </li>
+                  </ul>
+                  {/* menu footer  */}
+                  <ul className="flex justify-center 
+                  cursor-pointer items-center relative mr-10px gap-x-7 text-[28px]  ">
+                    <li> <a className=' social-icon' href="#"></a> Home </li>
+                    <li> <a className=' social-icon'  href="#"></a>About </li>
+                    <li> <a className=' social-icon'  href="#"></a>Services </li>
+                    <li> <a className=' social-icon'  href="#"></a> Work </li>
+                    <li> <a className=' social-icon'  href="#"></a>Contact </li>
+                  </ul>
+                  <p>© 2010-2023 Bizcope Digital Ltd. All Rights Reserved.</p>
+            </div>
+            
+        </footer>
+    );
+};
+
+export default Footer;
